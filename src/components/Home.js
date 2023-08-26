da24793cb27f6d5b264b6417a7a437348817ca0e
saurabhpawar005 <121420556+saurabhpawar005@users.noreply.github.com>
2023-08-26 08:07:38 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+         <img src="https://media.istockphoto.com/id/1157789866/photo/modern-computer-laptop-with-blank-screen-on-counter-barand-window-view.webp?b=1&s=170667a&w=0&k=20&c=KQvHmtg9VE7omt0okMYHQEKlBg9pneV_giSGY1KS3f0=" alt="" style={{width:"100%",height:"90vh"}}/>
+    </>
+  )
+}
+
+export default Home
